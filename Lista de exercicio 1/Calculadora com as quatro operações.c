6a9f62942cf7249bb1269a07d1bd5910b095852d
Hiury Manoel <hiurym123@gmail.com>
2026-09-10 21:33:15 -0300

    Exercicios feitos do 1 a 6, renomealos depois

---
+++ Lista de exercicio 1/Calculadora com as quatro operações.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+    int escolha;
+    float numero1, numero2, divisao, soma, sub, vezes, resultado;
+
+    printf("Digite o primeiro número: ");
+    scanf("%f", &numero1);
+    printf("Digite o primeiro número: ");
+    scanf("%f", &numero2);
+    printf("Escolha a operação: \n1 - soma\n2 - sub\n3 - vezes\n4 - divisao\n\n");
+    scanf("%d", &escolha);
+
+    if (numero2 == 0) {
+        if (escolha == 4) {
+        printf("\nNão é permitido dividar por zero");
+        return 0;
+        }
+    }
+
+    if (escolha == 1) {
+        resultado = numero1 + numero2;
+        printf("A soma dos numeros é: %f\n", resultado);
+    }
+
+    else if (escolha == 2) {
+        resultado = numero1 - numero2;
+        printf("A subtração dos numeros é: %f\n", resultado);
+    }
+
+    else if (escolha == 3) {
+        resultado = numero1 * numero2;
+        printf("A mutiplicação dos numeros é: %f\n", resultado);
+    }
+
+    else if (escolha == 4) {
+        resultado = numero1 / numero2;
+        printf("A divisão dos numeros é: %f\n", resultado);
+    }
+    
+    else printf("Nenhuma operação foi escolhida");
+    return 0;
+}
