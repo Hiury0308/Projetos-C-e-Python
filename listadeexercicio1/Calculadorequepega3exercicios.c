7b5a3f8b1a2d07692fc27df95a234d8b04564585
Hiury0308 <hiurym123@gmail.com>
2026-09-13 11:22:29 -0300

    Refactor calculator code for better readability

--- listadeexercicio1/Calculadorequepega3exercicios.c
+++ listadeexercicio1/Calculadorequepega3exercicios.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main() {
-
+int main() 
+{
+    //declaração dar variaveis
     int escolha;
-    float numero1, numero2, divisao, soma, sub, vezes, resultado;
+    float numero1, numero2, resultado;
 
+    //inputs do usuario
     printf("Digite o primeiro número: ");
     scanf("%f", &numero1);
     printf("Digite o primeiro número: ");
@@ -12,33 +14,37 @@ int main() {
     printf("Escolha a operação: \n1 - soma\n2 - sub\n3 - vezes\n4 - divisao\n\n");
     scanf("%d", &escolha);
 
-    if (numero2 == 0) {
-        if (escolha == 4) {
+    //Bloqueador para contas de divisor com 0
+    if (numero2 == 0) 
+    {
+        if (escolha == 4) 
+        {
         printf("\nNão é permitido dividar por zero");
         return 0;
         }
     }
 
-    if (escolha == 1) {
+    //Retorno dos resultados baseado na escolha do usuario
+    if (escolha == 1) 
+    {
         resultado = numero1 + numero2;
         printf("A soma dos numeros é: %f\n", resultado);
     }
-
-    else if (escolha == 2) {
+    else if (escolha == 2) 
+    {
         resultado = numero1 - numero2;
         printf("A subtração dos numeros é: %f\n", resultado);
     }
-
-    else if (escolha == 3) {
+    else if (escolha == 3) 
+    {
         resultado = numero1 * numero2;
         printf("A mutiplicação dos numeros é: %f\n", resultado);
     }
-
-    else if (escolha == 4) {
+    else if (escolha == 4) 
+    {
         resultado = numero1 / numero2;
         printf("A divisão dos numeros é: %f\n", resultado);
     }
-    
     else printf("Nenhuma operação foi escolhida");
     return 0;
 }
