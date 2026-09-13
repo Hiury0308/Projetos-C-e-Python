17fbea84bc97ccf7a1094dac25406a8d520147fd
Hiury0308 <hiurym123@gmail.com>
2026-09-13 11:23:48 -0300

    Update product value calculation over 3 years

--- listadeexercicio1/exe-10.c
+++ listadeexercicio1/exe-10.c
@@ -7,6 +7,10 @@ int main() {
     printf("Insira o valor do produto: ");
     scanf("%f", &valor);
 
-    printf("O valor ao decorres de 3 anos é: R$%.2f\n", (valor * 0.30));
+    printf("O valor ao decorrer de 3 anos considerando uma redução no valor original é: R$%.2f\n", valor - (valor * 0.30));
+    valor = valor - (valor * 0.10);
+    valor = valor - (valor * 0.10);
+    valor = valor - (valor * 0.10);
+    printf("O valor ao decorrer de 3 anos reduzinho sobre o novo valor é: %.2f\n", valor);
 
 }
