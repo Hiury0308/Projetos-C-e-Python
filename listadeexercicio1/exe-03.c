c4506667d17bf28f9c7fc70670fc017811afc3ba
Hiury0308 <hiurym123@gmail.com>
2026-09-13 11:22:48 -0300

    Implement basic number operations in exe-03.c

---
+++ listadeexercicio1/exe-03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    float numero;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &numero);
+    printf("O dobro do seu numero é: %.1f\n", numero * 2)
+    printf("A metade do seu numero é: %.1f\n", numero / 2)
+}
