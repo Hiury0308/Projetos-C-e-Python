0690b842bb236989fed8c9a91521c3b576f41e01
Hiury0308 <hiurym123@gmail.com>
2026-09-14 14:37:49 -0300

    Fix formatting and improve output messages in exe-04.c

--- listadeexercicio1/exe-04.c
+++ listadeexercicio1/exe-04.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
+    //Declaração das variaveis 
     float numero;
+
+    //Recebimento dos dados
     printf("Digite o primeiro número: ");
     scanf("%f", &numero);
 
+    //Saida dos resultados
     printf("O numero anterior é: %.1f\n", numero - 1);
     printf("O seu numero é número: %.1f\n", numero);
     printf("O proximo número é: %.1f\n", numero + 1);
