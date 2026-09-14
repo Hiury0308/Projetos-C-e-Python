d41fa40fde9c55adcfe5a7ae01ecbd7867b526ad
Hiury0308 <hiurym123@gmail.com>
2026-09-14 14:01:16 -0300

    Update exe-03.c

--- listadeexercicio1/exe-03.c
+++ listadeexercicio1/exe-03.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
+    //Declaração das variaveis
     float numero;
 
+    //Recebimento do valor pelo ususario
     printf("Digite a primeira nota: ");
     scanf("%f", &numero);
+
+    //saida para o usuario.
     printf("O dobro do seu numero é: %.1f\n", numero * 2)
     printf("A metade do seu numero é: %.1f\n", numero / 2)
 }
