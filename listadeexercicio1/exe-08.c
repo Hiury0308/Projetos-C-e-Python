df656d9f420bdf62909b83dfdef796901879bead
Hiury0308 <hiurym123@gmail.com>
2026-09-15 10:16:29 -0300

    Refactor exe-08.c for improved readability
    Refactor code for clarity and fix syntax errors.

--- listadeexercicio1/exe-08.c
+++ listadeexercicio1/exe-08.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+    //variaveis utilizadas no codigo
+    int dias, valor_para_conta, valor_com_desconto;
 
-    int dias, valorparaconta, valorcomdesconto;
-
+    //recebimento da quantia de dias
     printf("Digite a quantia de dias trabalhados: ");
     scanf("%d", &dias);
 
-    printf("Valor total: R$%.2d\n", valorparaconta = dias * 30 );
-    printf("Valor do descontos: R$%.2d\n", valorcomdesconto = valorparaconta / 10);
-    printf("Valor pós desconto: R$%.2d\n", valorparaconta - valorcomdesconto);
+    //Operações matematicas
+    valor_para_conta = dias * 30
+    valor_com_desconto = valor_para_conta / 10
+        
+    //saida para o usuario
+    printf("Valor total: R$%.2d\n", valor_para_conta);
+    printf("Valor do descontos: R$%.2d\n", valor_com_desconto);
+    printf("Valor pós desconto: R$%.2d\n", valor_para_conta - valor_com_desconto);
+    return 0;
 }
