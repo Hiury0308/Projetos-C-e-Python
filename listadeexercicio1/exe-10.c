1be6e97f3d2776d12f1f449ec82ffc0f2db472dd
Hiury0308 <hiurym123@gmail.com>
2026-09-15 10:36:39 -0300

    Refactor main function formatting in exe-10.c

--- listadeexercicio1/exe-10.c
+++ listadeexercicio1/exe-10.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int main() {
-
+int main() 
+{
+    //definição das variaveis
     float valor;
-    
+
+    //recebimento do valor pelo usuario
     printf("Insira o valor do produto: ");
     scanf("%f", &valor);
 
+    //processamento e saida dos resultados
     printf("O valor ao decorrer de 3 anos considerando uma redução no valor original é: R$%.2f\n", valor - (valor * 0.30));
     valor = valor - (valor * 0.10);
     valor = valor - (valor * 0.10);
     valor = valor - (valor * 0.10);
     printf("O valor ao decorrer de 3 anos reduzinho sobre o novo valor é: %.2f\n", valor);
-
+    return 0;
 }
