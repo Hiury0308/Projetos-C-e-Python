e98e1f5805268e86fc0d815638ca727b23532145
Hiury0308 <hiurym123@gmail.com>
2026-09-15 10:10:51 -0300

    Use #define for shirt price constants
    Refactor shirt pricing variables to use #define constants.

--- listadeexercicio1/exe-05.c
+++ listadeexercicio1/exe-05.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
-    int camisaP, camisaM, camisaG, camisaPValor, camisaMValor, camisaGValor, resultadoG, resultadoM, resultadoP;
+    //Variaveis usadas no codigo
+    #define camisa_p_valor 15 
+    #define camisa_m_valor 20 
+    #define camisa_g_valor 25
+    int camisaP, camisaM, camisaG,  resultadoG, resultadoM, resultadoP;
+
+    //Recebimento dos valores pelo usuario
     printf("Digite a quantidade de camisas Pequenas: ");
     scanf("%d", &camisaP);
     printf("Digite a quantidade de camisas Médias: ");
@@ -10,13 +17,12 @@ int main() {
     printf("Digite a quantidade de camisas Grandes: ");
     scanf("%d", &camisaG);
 
-    camisaPValor = 15;
-    camisaMValor = 20;
-    camisaGValor = 25;
-    
-    resultadoG = camisaG * camisaGValor;
-    resultadoM = camisaM * camisaMValor;
-    resultadoP = camisaP * camisaPValor;
-    
+    //Operações matematicas
+    resultadoG = camisaG * camisa_g_valor;
+    resultadoM = camisaM * camisa_m_valor;
+    resultadoP = camisaP * camisa_p_valor;
+
+    //saida do resultado
     printf("Seu total é: %.0d", resultadoG + resultadoM + resultadoP);
+    return 0;
 }
