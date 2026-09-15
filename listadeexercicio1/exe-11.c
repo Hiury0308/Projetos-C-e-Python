9464bf73d89c189b4e903370acdd7c69d518dbf1
Hiury0308 <hiurym123@gmail.com>
2026-09-15 10:38:04 -0300

    Refactor variable initialization and add comments

--- listadeexercicio1/exe-11.c
+++ listadeexercicio1/exe-11.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-int main() {
-
+int main() 
+{
+    //definição das variaveis
     float A, B, C;
 
+    //proteção inicial contra lixo de memoria
+    C = 0;
+
+    //Recebimentod dos dados pelo usuario
     printf("Digite o valor de A: ");
     scanf("%f", &A);
     printf("Digite o valor de B: ");
     scanf("%f", &B);
 
-
+    //processamento para troca dos valors
     C = A;
     A = B;
     B = C;
-    
+
+    //saida do resultado
     printf("Valor A: %f\n", A);
     printf("Valor B: %f\n", B);
+    return 0;
 }
     
